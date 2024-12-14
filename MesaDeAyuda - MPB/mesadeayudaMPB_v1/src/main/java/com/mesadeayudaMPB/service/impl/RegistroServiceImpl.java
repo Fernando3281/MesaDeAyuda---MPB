cc9fa70a4c057f8603ce75a6669074f6c644ec76
Fernando3281 <fervargas2003@hotmail.com>
2024-12-14 16:58:28 -0600

    update RegistroServiceImpl

--- MesaDeAyuda - MPB/mesadeayudaMPB_v1/src/main/java/com/mesadeayudaMPB/service/impl/RegistroServiceImpl.java
+++ MesaDeAyuda - MPB/mesadeayudaMPB_v1/src/main/java/com/mesadeayudaMPB/service/impl/RegistroServiceImpl.java
@@ -3,14 +3,8 @@ package com.mesadeayudaMPB.service.impl;
 import com.mesadeayudaMPB.dao.UsuarioDao;
 import com.mesadeayudaMPB.domain.Usuario;
 import com.mesadeayudaMPB.service.RegistroService;
-import com.mesadeayudaMPB.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
-import jakarta.mail.MessagingException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import jakarta.mail.MessagingException;
-import java.util.Locale;
 
 @Service
 public class RegistroServiceImpl implements RegistroService {
@@ -18,41 +12,12 @@ public class RegistroServiceImpl implements RegistroService {
     @Autowired
     private UsuarioDao usuarioDao;
 
-    @Autowired
-    private CorreoService correoService;
-
-    @Autowired
-    private UsuarioService usuarioService;
-
-    @Autowired
-    private MessageSource messageSource;
-
     @Override
-    public void registrarNuevoUsuario(Usuario usuario) throws MessagingException {
-        // Verifica si el usuario ya existe
+    public void registrarNuevoUsuario(Usuario usuario) {
         if (!usuarioDao.existsByNombreOrCorreoElectronico(usuario.getNombre(), usuario.getCorreoElectronico())) {
-            // Encripta la contraseña
-            var codigo = new BCryptPasswordEncoder();
-            usuario.setContrasena(codigo.encode(usuario.getContrasena()));
-
-            // Guarda el usuario como inactivo
-            usuario.setActivo(false);
             usuarioDao.save(usuario);
-
-            // Enviar correo de activación
-            enviaCorreoActivar(usuario);
-
         } else {
             throw new IllegalArgumentException("El usuario ya existe");
         }
-    }
-
-    private void enviaCorreoActivar(Usuario usuario) throws MessagingException {
-        // Aquí puedes agregar la lógica para enviar el correo de activación
-        String mensaje = String.format(
-                messageSource.getMessage("registro.mensaje.activacion.ok", null, Locale.getDefault()),
-                usuario.getCorreoElectronico());
-
-        correoService.enviarCorreo(usuario.getCorreoElectronico(), "Activación de cuenta", mensaje);
     }
 }
